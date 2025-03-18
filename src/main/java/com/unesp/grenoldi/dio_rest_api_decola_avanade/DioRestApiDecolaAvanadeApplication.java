e89698e2a875446f9235396f2cce80ec29101bfa
grenoldi <guilherme.renoldi@gmail.com>
2025-03-18 11:00:16 -0300

    first commit

---
+++ src/main/java/com/unesp/grenoldi/dio_rest_api_decola_avanade/DioRestApiDecolaAvanadeApplication.java
@@ -0,0 +1,13 @@
+package com.unesp.grenoldi.dio_rest_api_decola_avanade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioRestApiDecolaAvanadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioRestApiDecolaAvanadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unesp/grenoldi/dio_rest_api_decola_avanade/DioRestApiDecolaAvanadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unesp.grenoldi.dio_rest_api_decola_avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioRestApiDecolaAvanadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
